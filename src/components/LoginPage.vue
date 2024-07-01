ab5d1bf973dce11374e9f475d2587f94f23e3470
G4bizinha <gabivonborowski@gmail.com>
2024-06-30 23:31:10 -0300

    package

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <q-btn @click="mostrarFormulario = true" label="Login" />
   <q-dialog v-model="mostrarFormulario">
     <q-card>
@@ -10,23 +9,11 @@
       </q-card-section>
     </q-card>
   </q-dialog>
-=======
-  <div>
-    <q-input v-model="cpf" label="CPF" />
-    <q-input v-model="telefone" label="Telefone" type="password" />
-    <q-btn @click="fazerLogin" label="Login" color="primary" />
-  </div>
->>>>>>> d06f4e66622fbde085b9cf0d3f12397b3856a590
 </template>
 
 <script>
 import { defineComponent } from "vue";
-<<<<<<< HEAD
 import { getUserByCpf } from "src/services/CadastroService.js";
-=======
-import {getUserByCpf} from "src/services/CadastroService.js";
-
->>>>>>> d06f4e66622fbde085b9cf0d3f12397b3856a590
 
 export default defineComponent({
   name: "LoginPage",
@@ -34,30 +21,20 @@ export default defineComponent({
     return {
       cpf: "",
       telefone: "",
-<<<<<<< HEAD
       mostrarFormulario: false,
-=======
->>>>>>> d06f4e66622fbde085b9cf0d3f12397b3856a590
     };
   },
   methods: {
     async fazerLogin() {
       try {
-<<<<<<< HEAD
         getUserByCpf(this.cpf, this.telefone)
           .then(() => {
             console.log(response.data);
             this.$q.notify({
-=======
-        getUserByCpf(this.cpf, this.telefone).then(() => {
-          console.log(response.data);
-          this.$q.notify({
->>>>>>> d06f4e66622fbde085b9cf0d3f12397b3856a590
               color: "positive",
               message: "Login efetuado com sucesso!",
               position: "top",
             });
-<<<<<<< HEAD
           })
           .catch((error) => {
             console.error("Erro ao fazer login:", error);
@@ -67,16 +44,6 @@ export default defineComponent({
               position: "top",
             });
           });
-=======
-        }).catch((error) => {
-          console.error("Erro ao fazer login:", error);
-          this.$q.notify({
-            color: "negative",
-            message: "Erro ao fazer login. Por favor, tente novamente.",
-            position: "top",
-          });
-        });
->>>>>>> d06f4e66622fbde085b9cf0d3f12397b3856a590
       } catch (error) {
         console.error("Erro ao fazer login:", error);
         this.$q.notify({
@@ -91,23 +58,15 @@ export default defineComponent({
 </script>
 
 <style scoped>
-<<<<<<< HEAD
 .login {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100vh;
-  border: 7px solid #05ff00;
 }
-.border {
-  border: 2px solid #05ff00;
-}
+
 .btn {
   width: 12rem;
 }
 </style>
-=======
-/* Estilos específicos para o componente de login */
-</style>
->>>>>>> d06f4e66622fbde085b9cf0d3f12397b3856a590
